755b6dfe6a53cb7e6861c95d2c6bd4bf2ce01e15
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-24 13:12:40 +0300

    Задачв22

---
+++ Zadacha22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача на таблицу квадратов введенного числа. 
+Console.Clear();
+int GetNumber() // метод введения числа и возврата его в программу.
+{
+   Console.WriteLine("Введите число: ");
+   return Convert.ToInt32(Console.ReadLine());
+
+}
+void GetPrintPow(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+    Console.Write(Math.Pow((count),2) + " ");
+    count++;
+    }
+}
+GetPrintPow(GetNumber());
